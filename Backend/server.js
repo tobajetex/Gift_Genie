8f9bf1cd5ef601d36fa42a0128f59f0d0ad3a9b8
Jetawo Oloruntoba <jetawotobajetex@gmail.com>
2026-05-03 00:13:01 +0100

    update to file

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,16 @@
 import express from "express";
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+import path from "path";
+import { fileURLToPath } from "url";
 import { checkEnvironment } from "./checkEnvironment.js";
 import { giftController } from "./controllers/giftController.js";
 
 import cors from "cors";
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 //End of added code
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 checkEnvironment();
 
 app.post("/api/gift", giftController);
